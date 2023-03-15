e2926c32b58fe64d6d7bd93bbe6445aee6642f13
A.A <115159286+Ade-mir@users.noreply.github.com>
2023-03-15 16:27:04 +0100

    Update 2-e.js

--- Solutions/checkpoint-1/2-e.js
+++ Solutions/checkpoint-1/2-e.js
@@ -30,3 +30,5 @@ function printSequence(n, m) {
 }
 
 printSequence(1, 23);
+
+// For the last bonus question in this checkpoint I created two variables, and left them undefined. Then I handled the case where n is the same as m, and returned a string asking for different numbers. If the numbers are different I then handled the scenario where n might be bigger than m, and vice versa. now regardless of which number is smaller, the subsequent code works the same by setting setting the range values to be equal to the previously mentioned variables biggerNum and smallerNum. After that I used if else statments to check whether or not they are divisible by 3, 7, or both, followed by the stop condition.
