f6d1fa7c6e1c8413d079a077c5be8e62365e85fa
A.A <115159286+Ade-mir@users.noreply.github.com>
2023-03-20 14:15:20 +0100

    Added checkpoint 3 solutions.

---
+++ Solutions/checkpoint-3/level-1-a.js
@@ -0,0 +1,17 @@
+// In JavaScript, there are six primitive data types that are used to represent basic values:
+// 1. Boolean: The Boolean data type represents a logical value of true or false.
+// 2. Null: The Null data type represents a deliberate non-value, used to indicate that a variable has no value.
+// 3. Undefined: The Undefined data type represents a variable that has been declared but has not been assigned a value.
+// 4. Number: The Number data type represents a numeric value, including integers and floating-point numbers.
+// 5. String: The String data type represents a sequence of characters, enclosed in quotes.
+// 6. Symbol: The Symbol data type represents a unique identifier, used primarily as the key for object properties.
+
+// These primitive data types are immutable, which means that their values cannot be changed once they are created. Other data types in JavaScript, such as objects and arrays, are mutable and can be changed after they are created.
+
+//Examples of the six types of primitive values.
+let bool = true;
+let nothing = null;
+let undef;
+let num = 1;
+let str = "hello";
+let sym = Symbol();
---
+++ Solutions/checkpoint-3/level-1-c.js
@@ -0,0 +1,6 @@
+function getSecondWord(stringOfWords) {
+  var words = stringOfWords.split(" ");
+  return words[1];
+}
+
+console.log(getSecondWord("hello my good friend"));
